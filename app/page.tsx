db77f07888be01bbedb975757fe542bbd7f6fbe3
Will Hopkins <willothyh@gmail.com>
2024-11-12 16:45:30 -0800

    add shadcn ui and sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
+
 export default function Home() {
-  return <main>{"todo"}</main>;
+  return (
+    <SidebarProvider>
+      <Sidebar>
+        <SidebarHeader>implement me</SidebarHeader>
+        <SidebarContent className="flex items-center justify-center">
+          implement me
+        </SidebarContent>
+        <SidebarFooter>implement me</SidebarFooter>
+      </Sidebar>
+      <main className="w-full flex items-center justify-center">
+        implement me
+      </main>
+    </SidebarProvider>
+  );
 }
