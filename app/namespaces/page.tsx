c7612bd833f9e847df4a05ba19b9d0e9955fec55
David Chu <david@fetchflow.ai>
2024-11-14 15:44:12 -0800

    feat: added createNamespace, added TanstackQuery to Providers

--- app/namespaces/page.tsx
+++ app/namespaces/page.tsx
@@ -1,12 +1,28 @@
-"use server";
+"use client";
 import { listNamespaces } from "@/actions/api";
 import { NamespaceTable } from "@/components/namespaces/table";
+import CreateNamespace from "@/components/create-namespace";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
-export default async function Namespaces() {
-  const data = await listNamespaces();
-  console.log(data);
+export default function Namespaces() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["list-namespaces"],
+    queryFn: listNamespaces,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading namespaces</div>;
+
   return (
-    <div className="h-full flex">
-    <NamespaceTable data={data}/></div>
+    <div className="h-full flex flex-col gap-4">
+      <NamespaceTable data={data || []} />
+      <div className="flex justify-end px-4">
+        <Button onClick={() => setIsOpen(true)}>Create Namespace</Button>
+      </div>
+      <CreateNamespace open={isOpen} close={() => setIsOpen(false)} />
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { listQueues } from "@/actions/api";
 import { QueuesTable } from "@/components/queues/table";
 
+
 export default async function Queues() {
   const data = await listQueues();
   console.log(data);
