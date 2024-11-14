39f38f999f52e7f2972ce313bc3c688d3d948418
Will Hopkins <willothyh@gmail.com>
2024-11-13 19:59:52 -0800

    ui pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { cookies } from "next/headers";
 import Providers from "./providers";
+import DashboardSidebar from "@/components/sidebar";
 
 export const metadata: Metadata = {
   title: "Creek UI",
@@ -19,7 +20,11 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Providers sidebarOpen={defaultOpen}>{children}</Providers>
+        <Providers sidebarOpen={defaultOpen}>
+          <DashboardSidebar />
+
+          <main className="w-full min-h-svh">{children}</main>
+        </Providers>
       </body>
     </html>
   );
