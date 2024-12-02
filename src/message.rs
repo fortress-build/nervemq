445127a4839cc5d7ac01ff6eaf279a1bcef06258
David Chu <77809311+dchu917@users.noreply.github.com>
2024-12-02 01:47:32 -0800

    refactor: remove some columns from queue List, make medium screen size not horizontal scroll (#27)

--- src/message.rs
+++ src/message.rs
@@ -3,6 +3,20 @@ use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
 
+#[derive(Serialize, Deserialize, Debug, sqlx::Type)]
+#[sqlx(type_name = "text")]
+pub enum MessageStatus {
+    #[serde(rename = "pending")]
+    #[sqlx(rename = "pending")]
+    Pending,
+    #[serde(rename = "delivered")]
+    #[sqlx(rename = "delivered")]
+    Delivered,
+    #[serde(rename = "failed")]
+    #[sqlx(rename = "failed")]
+    Failed,
+}
+
 #[derive(Serialize, Deserialize, FromRow)]
 pub struct Message {
     pub id: u64,
@@ -11,6 +25,9 @@ pub struct Message {
     pub delivered_at: Option<u64>,
     pub sent_by: Option<u64>,
     pub body: Vec<u8>,
+    pub tries: u64,
+
+    pub status: MessageStatus,
 
     #[sqlx(skip)]
     pub kv: HashMap<String, String>,
