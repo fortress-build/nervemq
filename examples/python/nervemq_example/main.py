41fcbbf24ae511092fc5192db777e0ec8efb3578
David Chu <77809311+dchu917@users.noreply.github.com>
2024-12-05 18:22:45 -0800

    draft for js example (#32)

--- examples/python/nervemq_example/main.py
+++ examples/python/nervemq_example/main.py
@@ -7,8 +7,8 @@ host_url = 'http://localhost:8080/sqs'
 def main():
     sqs: SQSClient = boto3.client(
         'sqs',
-        aws_access_key_id='i71gSbEkGs7',
-        aws_secret_access_key='NqJZg1wuPaCzLgrzNC1fydNNrkgtKDQnD',
+        aws_access_key_id='ZBcnTSTKX69',
+        aws_secret_access_key='38zBir4Vvn2SKAx6VpPAvdNY4LzBaBGBQ',
         region_name='us-west-1',
         endpoint_url=host_url,
     )
